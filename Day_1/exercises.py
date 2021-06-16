e92160afa23fc1969bc9686073c9b3c3224efc61
“hasimengin <hasimengin@hotmail.com>
2021-06-16 09:28:08 -0400

    "first commit"

---
+++ Day_1/exercises.py
@@ -0,0 +1,28 @@
+
+
+# Day 1 exercises:
+
+#1 - Create a variable named carname and assign the value Volvo to it.
+
+
+#2 -Create a variable named x and assign the value 50 to it.
+
+#3-Print the sum of 5 + 10, using two variables: x and y.
+
+#4-Remove the illegal characters in the variable name below:
+#2my-first_name = "John"
+#5-Print the data type of x:
+x=5
+#6-Convert x into a  string.
+x=7
+
+#8-  Accept two numbers from the user and calculate multiplication:
+
+# 9- Ask a user to enter his/her name , last name and date of birth, and display user inputs
+# expected result should be :
+  # My name is Jonn.
+  # My last name is Smith.
+  # I am 35 years old.
+
+# 10-import random module and generate a random variable by using random.radom() function
+#from random module(library)
