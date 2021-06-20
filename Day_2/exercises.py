5b5b4047a2772b72b0175d0f2545bfa76b0138b8
“hasimengin <hasimengin@hotmail.com>
2021-06-19 22:27:34 -0400

    "first commit"

---
+++ Day_2/exercises.py
@@ -0,0 +1,24 @@
+
+#1- Remove 20 from the list below.
+sampleList = [10, 20, 30, 40, 50]
+
+#2-merge the list below.
+listOne  =  ['a', 'b', 'c', 'd']
+listTwo =  ['e', 'f', 'g']
+
+#3- create a nested list from the lists below
+listOne  =  ['a', 'b', 'c', 'd']
+listTwo =  ['e', 'f', 'g']
+
+#4-imput list >>aList = [4, 8, 12, 16].Conver alist to  [4, 20, 24, 28]
+
+
+#5- Add 60 to the list below
+sampleList = [10, 20, 30, 40, 50]
+
+#6- Multiple all items with 2 in the list below:
+
+aList = [1, 2, 3, 4, 5, 6, 7]
+
+#7- Get only last 3 items from the list below:
+aList = [10, 20, 30, 40, 50, 60, 70, 80]
